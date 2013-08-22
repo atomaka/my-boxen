04b0e2a113a2b12d02c0a734fb8200552b80925f
Andrew Tomaka <atomaka@gmail.com>
2013-08-22 13:49:17 -0400

    Add some development stuff

--- modules/people/manifests/atomaka.pp
+++ modules/people/manifests/atomaka.pp
@@ -7,9 +7,13 @@ class people::atomaka {
   include zsh
   include adium
   include thunderbird
+  include virtualbox
+  include vagrant
+  include wget
   include skype
 
   package { 'coreutils': }
+  package { 'tig': }
 
   include atomaka::dotfiles
   include atomaka::osx::config
