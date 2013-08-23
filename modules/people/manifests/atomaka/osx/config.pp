a3fffa23ab7e69d44993566f26cf02f9d695a4e0
Andrew Tomaka <atomaka@gmail.com>
2013-08-23 01:32:52 -0400

    Setup OSX time

--- modules/people/manifests/atomaka/osx/config.pp
+++ modules/people/manifests/atomaka/osx/config.pp
@@ -5,4 +5,16 @@ class people::atomaka::osx::config {
 
   include osx::finder::show_all_on_desktop
   include osx::dock::clear_dock
+
+  boxen::osx_defaults { 'Change Time Format':
+    user   => $::boxen_user,
+    domain => 'com.apple.menuextra.clock',
+    key    => 'DateFormat',
+    value  => 'MMM d H:mm',
+    notify => Exec['killall SystemUIServer'];
+  }
+
+  exec { 'killall SystemUIServer':
+    refreshonly => true,
+  }
 }
