7d261c83a76e03eecb9a0fd38403b0a9c33ef2a6
Andrew Tomaka <atomaka@gmail.com>
2014-05-14 01:10:24 -0400

    Remove unused modules

--- modules/people/manifests/atomaka/applications.pp
+++ modules/people/manifests/atomaka/applications.pp
@@ -8,19 +8,14 @@ class people::atomaka::applications {
   include flux
   include heroku
   include iterm2::stable
-  include league_of_legends::na
-  #include libreoffice
   include python
   include skype
-  include spotify
   include sublime_text_2
   include tmux
-  include tunnelblick
   include qt
   include vagrant
   include virtualbox
   include vlc
   include wget
-  include whatpulse
   include zsh
 }
